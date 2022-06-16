a433fe1c16a29f58239630b91070d4b64dc6f735
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2022-06-16 12:20:48 +0200

    remove overview

--- app/src/main/java/se/payerl/noted/MainActivity.kt
+++ app/src/main/java/se/payerl/noted/MainActivity.kt
@@ -3,31 +3,24 @@ package se.payerl.noted
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.util.Log
-import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.res.ResourcesCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.findNavController
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import se.payerl.noted.fragments.AboutFragment
-import se.payerl.noted.fragments.AboutFragmentDirections
-import se.payerl.noted.fragments.OverviewFragment
-import se.payerl.noted.fragments.OverviewFragmentDirections
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var abdt: ActionBarDrawerToggle
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navView = drawer.findViewById<NavigationView>(R.id.nav_view)
 
@@ -39,8 +32,8 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
             when(it.itemId) {
-                R.id.drawer_list_overview -> findNavController(R.id.nav_host_fragment).navigate(NavGraphDirections.globalToAboutFragment())
-                R.id.drawer_about -> findNavController(R.id.nav_host_fragment).navigate(NavGraphDirections.globalToOverviewFragment())
+                R.id.drawer_list_root -> findNavController(R.id.nav_host_fragment).navigate(NavGraphDirections.globalToListViewFragment())
+                R.id.drawer_about -> findNavController(R.id.nav_host_fragment).navigate(NavGraphDirections.globalToAboutFragment())
             }
             drawer.close()
             true
